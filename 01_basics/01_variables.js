4b760cdcf0f70e5c5b1cbe5556c3e688faf4d889
somyadeep17 <166922294+somyadeep17@users.noreply.github.com>
2024-04-19 16:07:53 +0000

    variables constant

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 14453
+let accountemail = "mehtasomya@gmail.com"
+var password = "8840"
+accountcity = "Chandigarh"
+
+// accountId = 2
+
+accountemail = "sdmehta17@gmail.com"
+password = "50517253"
+accountcity = "Lakhimpur"
+
+console.table([accountId, accountemail, password, accountcity])
+
