3f7af5c543b9a4d60312b36823013db2e47efc53
somyadeep17 <166922294+somyadeep17@users.noreply.github.com>
2024-04-23 16:15:22 +0000

    datatypesSummary

---
+++ 01_basics/datatypesSummary.js
@@ -0,0 +1,12 @@
+const score = 100;
+const scorevalue = 100.5;
+const name = "123";
+
+const temp = null;
+const value = true;
+
+let usermail;
+const id = Symbol("123")
+
+const object = 55;
+console.log(typeof id);
