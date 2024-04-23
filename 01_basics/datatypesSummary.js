f341f2393315a26ab88aea586fc20566f222ebc2
somyadeep17 <166922294+somyadeep17@users.noreply.github.com>
2024-04-23 20:17:53 +0000

    dataTypes summary

--- 01_basics/datatypesSummary.js
+++ 01_basics/datatypesSummary.js
@@ -1,12 +1,35 @@
-const score = 100;
-const scorevalue = 100.5;
-const name = "123";
+// const score = 100;
+// const scorevalue = 100.5;
+// const name = "123";
 
-const temp = null;
-const value = true;
+// const temp = null;
+// const value = true;
 
-let usermail;
-const id = Symbol("123")
+// let usermail;
+// const id = Symbol("123")
 
-const object = 55;
-console.log(typeof id);
+// const object = 55;
+// console.log(typeof id);
+
+// +++++++++++++++++++++++++
+
+// Stack (Primitive)  Heap (Non-Primitive)
+
+let myyoutubename = "Somyadeep"
+
+let anothername = myyoutubename
+anothername = "Hatim"
+
+console.log(anothername)
+
+let userOne = {
+    email: "mehtasomyadeep17@gmail.com",
+    upi: "sdm@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "sdmehta17@gmail.com"
+
+console.log(userOne.email)
+console.log(userTwo.email)
