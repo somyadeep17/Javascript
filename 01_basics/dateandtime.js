a7ff9cb4bd72395ec3aea7aa47afe567a4921d21
somyadeep17 <166922294+somyadeep17@users.noreply.github.com>
2024-07-09 09:14:00 +0000

    dateandtime

---
+++ 01_basics/dateandtime.js
@@ -0,0 +1,15 @@
+//DATES//
+ 
+// let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toDateString())
+// console.log(myDate. toISOString())
+// console.log(typeof myDate);
+
+//incase apni koi date deni hai//
+
+// let mycreatedDate = new Date(2000,9,17);
+// console.log(mycreatedDate. toDateString())
+
+let mycreatedDate = new Date(2000,9,17,6,2);
+console.log(mycreatedDate. toLocaleString());
