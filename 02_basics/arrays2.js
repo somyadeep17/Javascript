2719cc02e425dd0ecc5179011b12437f0f4de254
somyadeep17 <166922294+somyadeep17@users.noreply.github.com>
2024-07-10 08:55:35 +0000

    a

--- 02_basics/arrays2.js
+++ 02_basics/arrays2.js
@@ -3,3 +3,11 @@ const dc_heros = ["Flash", "Superman", "Batman"]
 
 const all_heros = [...marvel_heros, ...dc_heros]
 console.log(all_heros)
+
+const another_array = [1,2,3,[4,5],6,7,[6,7],4,5]
+const real_another_array = another_array.flat(Infinity)
+
+console.log(real_another_array);
+
+console.log(Array.isArray("Somyadeep"))
+console.log(Array.from("Somyadeep"))
