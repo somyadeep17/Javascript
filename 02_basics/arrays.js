9baf36b9f864afa390923d432d2052ba5565553f
somyadeep17 <166922294+somyadeep17@users.noreply.github.com>
2024-07-10 08:34:40 +0000

    02_basics

---
+++ 02_basics/arrays.js
@@ -0,0 +1,18 @@
+ const arr = [1,2,3,4,5]
+//  console.log(arr);
+
+// //  const arr2 =  new array [6,7,8,9]
+// //  console.log(arr2)
+
+// arr.push(8)
+// console.log(arr)
+
+console.log("A", arr)
+const myn1 = arr.slice(0,2)
+console.log(myn1)
+// console.log(arr)
+
+
+// console.log("B", arr)
+const myn2 = arr.splice(0,2)
+console.log(myn2)
---
+++ 02_basics/arrays2.js
@@ -0,0 +1,5 @@
+const marvel_heros = ["Thor", "Ironman", "Thanos"]
+const dc_heros = ["Flash", "Superman", "Batman"]
+
+const all_heros = [...marvel_heros, ...dc_heros]
+console.log(all_heros)
