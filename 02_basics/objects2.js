bc92c84cf5a4010e007bc972b7dcae3280ac1419
somyadeep17 <166922294+somyadeep17@users.noreply.github.com>
2024-07-12 10:50:19 +0000

    objects2.js

--- 02_basics/objects2.js
+++ 02_basics/objects2.js
@@ -19,13 +19,22 @@ const regularUser = {
 
 // console.log(regularUser.fullname.userfullname);
 
-const obj1 = {1: "a", 2: "b"}
-const obj2 = {3: "a", 4: "b"}
+// const obj1 = {1: "a", 2: "b"}
+// const obj2 = {3: "a", 4: "b"}
 
-// const obj3 = Object.assign(obj1,obj2)
-const obj3 = {...obj1,...obj2}
+// // const obj3 = Object.assign(obj1,obj2)
+// const obj3 = {...obj1,...obj2}
 
-console.log(obj3)
+// console.log(obj3)
 
-console.log(Object.keys(tinderUser))
 
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    instructor: "somyadeep",
+}
+
+console.log(course.coursename)
+const {price,instructor} = course
+console.log(price,instructor)
