c0536d9c16e740f4775d4b9e1f80f7ed050df2e4
somyadeep17 <166922294+somyadeep17@users.noreply.github.com>
2024-07-12 16:21:48 +0000

    functions.js

---
+++ 03_basics/functions.js
@@ -0,0 +1,18 @@
+function addtwonumbers (number1, number2){
+    // console.log(number1 + number2)
+    let result = number1+number2
+    return result
+}
+
+// const result = addtwonumbers(3,5)
+
+// console.log(result)
+
+function calculatecartprize (...num1){
+        return num1
+}
+// ... rest operator
+// console.log(calculatecartprize(200,300,5000,4000))
+
+const mynewArray = [200,300,400,500]
+function returnSecondvalue(getarray)
