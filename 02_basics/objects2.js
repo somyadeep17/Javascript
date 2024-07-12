8db96ec7e7120c7ff2b4d2e5fd05785e1efd00e0
somyadeep17 <166922294+somyadeep17@users.noreply.github.com>
2024-07-12 07:47:20 +0000

    objects2.js

---
+++ 02_basics/objects2.js
@@ -0,0 +1,31 @@
+// const tinderUser = new Object() singleton object
+const tinderUser = {}
+
+    tinderUser.id = "123abc"
+    tinderUser.name = "Hatim"
+    tinderUser.isLoggedin = false;
+
+// console.log(tinderUser)
+
+const regularUser = {
+    email: "ballumehta31@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "Somyadeep",
+            lastname: "Mehta"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+
+// const obj3 = Object.assign(obj1,obj2)
+const obj3 = {...obj1,...obj2}
+
+console.log(obj3)
+
+console.log(Object.keys(tinderUser))
+
