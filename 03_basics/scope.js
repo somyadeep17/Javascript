eed075c235066f30c32bf68b216879e37b0cec43
somyadeep17 <166922294+somyadeep17@users.noreply.github.com>
2024-07-13 11:43:14 +0000

    scope.js

---
+++ 03_basics/scope.js
@@ -0,0 +1,12 @@
+function one (){
+    const username = "somyadeep"
+
+    function two (){
+        const website = "youtube"
+        console.log(username)
+    }
+
+    // console.log(website)
+    two(username)
+}
+one ()
