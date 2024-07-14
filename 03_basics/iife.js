4f88e0bc6cd45e96c75f5d64da6ba1970d52a303
somyadeep17 <166922294+somyadeep17@users.noreply.github.com>
2024-07-14 09:15:41 +0000

    iife.js

---
+++ 03_basics/iife.js
@@ -0,0 +1,20 @@
+// Immediately invoked function Expressions (IIFE) 
+
+function  chai (){
+    console.log(`DB Connected`)
+}
+chai();
+
+(function chaiorcode (){
+    console.log(`DB Connected Two`);
+}) ();
+
+( () => {
+    console.log('DB Connected Three')
+} ) ();
+
+// agar IIFE fuction mein parameter pass krna hai to 
+
+( (name ) => {
+    console.log( `DB connected ${name}`);
+ } ) ('Somya deep')
