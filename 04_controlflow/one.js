40824fdb098d3ca3bf2d40d55764a4d43f93cfef
somyadeep17 <166922294+somyadeep17@users.noreply.github.com>
2024-07-15 08:55:26 +0000

    one.js

---
+++ 04_controlflow/one.js
@@ -0,0 +1,10 @@
+const temperature = 85
+if(temperature == 55){
+    console.log("temperature is less than 50")
+} else {
+    console.log("temperature is greater than 50")
+}
+
+// falsy values
+// false, 0 ,-0,BigInt On, "" , null, undefined, Nan
+// jo bhii values string ke under aa jaati hai vo true ho jaati hai 
